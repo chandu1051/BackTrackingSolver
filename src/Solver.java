122e51f290ee0795851e16da5167cf7af8662733
chandu1051 <chandu1051@gmail.com>
2018-03-09 20:08:45 +0530

    Create Solver.java
    Main program filw

---
+++ src/Solver.java
@@ -0,0 +1,29 @@
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/*
+Here we use back tracking and forward checking to check all possible combinations 
+
+Due to the large size the output can be stored under "BigInteger" class 
+
+
+*/
+
+class QuizGenerator  {
+
+public int generateQuizzes (  ) {
+    
+    
+    
+    
+    
+}
+
+
+	public static void main (String[] args) throws java.lang.Exception
+	{
+
+	}
+}
